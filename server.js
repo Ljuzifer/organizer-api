f0535e35c7f31147327bcc0df85f0417c656ccab
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-11-07 19:49:55 +0200

    xxxx

--- server.js
+++ server.js
@@ -5,33 +5,33 @@ mongoose.set("strictQuery", true);
 
 const { DB_HOST } = process.env;
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000);
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
-
-// async function run() {
-//   try {
-//     await mongoose.connect(DB_HOST);
-//     app.listen(3000, () => {
-//       console.log("Server running. Use our API on port: 3000");
-//     });
-//   } catch (error) {
-//     console.error(error.message);
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(3000);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
 //     process.exit(1);
-//   } finally {
-//     mongoose.disconnect();
-//   }
-// }
+//   });
 
-// run()
-//   .then(() => console.log("Database connection successful"))
-//   .catch((error) => console.error(error.message));
+async function run() {
+  try {
+    await mongoose.connect(DB_HOST);
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  } finally {
+    mongoose.disconnect();
+  }
+}
+
+run()
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.error(error.message));
 
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
