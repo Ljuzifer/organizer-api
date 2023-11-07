1f93aabafd7eacde158bae4dde3bd3510d83cdfc
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-11-07 19:10:52 +0200

    mongoose.connect

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+
+const { DB_HOST } = process.env;
+
+async function run() {
+  try {
+    await mongoose.connect(DB_HOST);
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  } finally {
+    mongoose.disconnect();
+  }
+}
+
+run()
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.error(error.message));
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+
+// const DB_HOST = `mongodb+srv://ljuzifer:X5bEZdd3Z2nIPKD8@cluster-organizer.z1ldooq.mongodb.net/phonebook_organizer
+// ?retryWrites=true&w=majority`;
+// X5bEZdd3Z2nIPKD8
